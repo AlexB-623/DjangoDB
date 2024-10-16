3c86e1a9a04c105aa19c6ef786fe05a933e39bc8
alexb <alex.brown577@gmail.com>
2024-10-16 15:15:20 -0400

    Day 1.5

--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -1,8 +1,10 @@
+from dataclasses import replace
 from wsgiref.validate import validator
-
+from django.urls import reverse
 from django.db import models
 from django.forms import CharField
 from django.core.validators import MinValueValidator, MaxValueValidator
+from django.utils.text import slugify
 
 
 # Create your models here.
@@ -12,6 +14,17 @@ class Book(models.Model):
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
+    slug = models.SlugField(default="", null=False)
+
+
+    def get_absolute_url(self):
+        return reverse("book-detail", args=[self.slug])
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
